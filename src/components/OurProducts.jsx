11efe7473f3bcaf5f45c3ce260ad1a78f82034df
kizya007 <kizya007@mail.ru>
2023-05-15 19:39:10 +0200

    spisok

--- src/components/OurProducts.jsx
+++ src/components/OurProducts.jsx
@@ -6,7 +6,9 @@ function OurProducts() {
         <div className="products">
             <span className="products-title">Наши товары</span>
             <div className="products-list">
-                <Binocular />
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
                 <div className="products-list__item">
                     <ul>
                         <li>Бинокли</li>
@@ -17,7 +19,159 @@ function OurProducts() {
                         <li>Для охоты и рыбалки</li>
                         <li>Любительский</li>
                         <li>Для профессионалов</li>
-                        <button>Показать все</button>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="products-list">
+                <div className="products-list__icon" >
+                    <Binocular />
+                </div>
+                <div className="products-list__item">
+                    <ul>
+                        <li>Бинокли</li>
+                        <li>Армейско-полевой</li>
+                        <li>Астрономический</li>
+                        <li>Для города</li>
+                        <li>Для детей</li>
+                        <li>Для охоты и рыбалки</li>
+                        <li>Любительский</li>
+                        <li>Для профессионалов</li>
+                        <button className="products-list__btn">Показать все <div className="btn-circle"><div className="btn-circle__item"></div><div className="btn-circle__item"></div><div className="btn-circle__item"></div></div></button>
                     </ul>
 
                 </div>
